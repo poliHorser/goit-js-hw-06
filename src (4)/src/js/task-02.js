97f5093a539d0a3d50f538d4213cf67f2dea0dc7
poliHorser <dzhulietta.horser@gmail.com>
2023-10-04 15:16:18 +0300

    .

--- src (4)/src/js/task-02.js
+++ src (4)/src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulIngredients = document.querySelector('#ingredients');
+
+const liElements = ingredients.map(ingredient => {
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.classList.add('item');
+  return li;
+});
+
+ulIngredients.append(...liElements);
